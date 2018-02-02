66fd9a2ef82b1d20eb2aaecbb876e9bc71f77bfb
Natalie <natabat@gmail.com>
2018-02-01 19:07:20 -0800

    new IPs

--- python_scripts/tv_power_and_source.py
+++ python_scripts/tv_power_and_source.py
@@ -5,8 +5,8 @@
 source_command = data.get('source_command')
 
 # turn on the tv
-input_data = {'entity_id': 'input_boolean.tv'}
-hass.services.call('input_boolean', 'turn_on', input_data)
+input_data = {'entity_id': 'switch.tv_power'}
+hass.services.call('switch', 'turn_on', input_data)
 
 # run the given rest command
 hass.services.call('rest_command', source_command)
