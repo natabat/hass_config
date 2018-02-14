9e3b666dbd6e428c0a1e3c43bda83438da40d377
Natalie <natabat@gmail.com>
2018-02-13 20:08:05 -0800

    porch light, better tv, more floorplan

--- python_scripts/tv_power_and_source.py
+++ python_scripts/tv_power_and_source.py
@@ -4,9 +4,14 @@
 # get params
 source_command = data.get('source_command')
 
-# turn on the tv
-input_data = {'entity_id': 'switch.tv_power'}
-hass.services.call('switch', 'turn_on', input_data)
+tv_switch_entity = 'switch.tv_power'
+states = hass.states.get(tv_switch_entity)
+
+while states.state == 'off':
+	# turn on the tv
+	input_data = {'entity_id': tv_switch_entity}
+	hass.services.call('switch', 'turn_on', input_data)
+	states = hass.states.get(tv_switch_entity)
 
 # run the given rest command
 hass.services.call('rest_command', source_command)
